2df70a26b491ee7cb767061c1c65cc6306e6cfc1
Houtan Bastani <houtan@dynare.org>
2017-08-02 17:31:11 -0400

    print out to clarify output

--- tests/julia_derivative_test/run_timing_test.jl
+++ tests/julia_derivative_test/run_timing_test.jl
@@ -1,6 +1,12 @@
+
+println("......ignore this......")
 include("test_symbolic_derivs.jl");
 include("test_preprocessor_derivs.jl");
 
+println()
+println("IGNORE WHAT IS ABOVE THIS LINE")
+println()
+println()
 println("-------------------------")
 println("- Symbolic Derivs Below -")
 println("-------------------------")
