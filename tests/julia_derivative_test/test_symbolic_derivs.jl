8a3b6953d4fc4377fa316207bd276933efe72a5a
Houtan Bastani <houtan@dynare.org>
2017-08-03 17:47:00 -0400

    adjust timing test for recent changes….

--- tests/julia_derivative_test/test_symbolic_derivs.jl
+++ tests/julia_derivative_test/test_symbolic_derivs.jl
@@ -1,13 +1,18 @@
-(json, model, StaticG1, staticg1ref, StaticG2, staticg2ref, DynamicG1, dynamicg1ref, DynamicG2, dynamicg2ref) = include("runtest.jl");
+(json, model, StaticG1!, staticg1ref, StaticG2!, staticg2ref, DynamicG1!, dynamicg1ref, DynamicG2!, dynamicg2ref) = include("runtest.jl");
 
-endogs = [i*1.0 for i=1:20];
-exogs = [i*1.0 for i=1:20];
-params = [i*1.0 for i=1:20];
+endogsd = [i*1.0 for i=1:12];
+endogss = [i*1.0 for i=1:6];
+exogs = [i*1.0 for i=1:2];
+params = [i*1.0 for i=1:7];
 
+sg1 = spzeros(6, 6)
+sg2 = spzeros(6, 36)
+dg1 = spzeros(6, 14)
+dg2 = spzeros(6, 196)
 N = 10000
 for i = 1:N
-    sg1 = StaticG1(endogs, exogs, params)
-    sg2 = StaticG2(endogs, exogs, params)
-    dg1 = DynamicG1(endogs, exogs, params)
-    dg2 = DynamicG2(endogs, exogs, params)
+    StaticG1!(endogss, exogs, params, sg1)
+    StaticG2!(endogss, exogs, params, sg2)
+    DynamicG1!(endogsd, exogs, params, dg1)
+    DynamicG2!(endogsd, exogs, params, dg2)
 end
